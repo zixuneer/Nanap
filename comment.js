7b296c3bfb7a92114b975fc9bb33c2fb958cdd9d
Huang Zixun <42267068+zixuneer@users.noreply.github.com>
2019-06-27 14:22:12 +0200

    Add files via upload

--- comment.js
+++ comment.js
@@ -15,67 +15,70 @@ function draw() {
   textFont('Yantramanav');
   textAlign(CENTER);
   fill(100);
-  text('About', 645, 120);
-  text('How to Use', 765, 120);
-  text('Comments', 885, 120);
-  text('Customize', 1005, 120);
+  text('About', width/2.13+45, height/6.6+20);
+  text('How to Use', width/2.13+165, height/6.6+20);
+  text('Comments', width/2.13+285, height/6.6+20);
+  text('Customize', width/2.13+405, height/6.6+20);
 
   mouse();
 }
 
 function mouse(){
-if(mouseX>600&&mouseX<690&&mouseY>100&&mouseY<130){
-  fill('#E3A72F');
-  noStroke();
-  rect(600, 100, 90, 30, 15);
-  fill(255);
-  text('About', 645, 120);
-}
+  if(mouseX>width/2.13&&mouseX<width/2.13+90&&mouseY>height/6.6&&mouseY<height/6.6+30){
+    fill('#E3A72F');
+    noStroke();
+    //rect(600, 100, 90, 30, 15);
+    rect(width/2.13, height/6.6, 90, 30, 15);
+    fill(255);
+    text('About', width/2.13+45, height/6.6+20);
+  }
 
+  if(mouseX>width/2.13+120&&mouseX<width/2.13+210&&mouseY>height/6.6&&mouseY<height/6.6+30){
+    fill('#058789');
+    noStroke();
+    //rect(720, 100, 90, 30, 15);
+    rect(width/2.13+120, height/6.6, 90, 30, 15);
+    fill(255);
+    text('How to Use', width/2.13+165, height/6.6+20);
+  }
 
-if(mouseX>720&&mouseX<810&&mouseY>100&&mouseY<130){
-  fill('#058789');
-  noStroke();
-  rect(720, 100, 90, 30, 15);
-  fill(255);
-  text('How to Use', 765, 120);
-}
+  if(mouseX>width/2.13+240&&mouseX<width/2.13+330&&mouseY>height/6.6&&mouseY<height/6.6+30){
+    fill('#D54B1A');
+    noStroke();
+    //rect(840, 100, 90, 30, 15);
+    rect(width/2.13+240, height/6.6, 90, 30, 15);
+    fill(255);
+    text('Comments', width/2.13+285, height/6.6+20);
+  }
 
-if(mouseX>840&&mouseX<930&&mouseY>100&&mouseY<130){
-  fill('#D54B1A');
-  noStroke();
-  rect(840, 100, 90, 30, 15);
-  fill(255);
-  text('Comments', 885, 120);
-}
-
-if(mouseX>960&&mouseX<1050&&mouseY>100&&mouseY<130){
-  fill('#503D2E');
-  noStroke();
-  rect(960, 100, 90, 30, 15);
-  fill(255);
-  text('Customize', 1005, 120);
-}
+  if(mouseX>width/2.13+360&&mouseX<width/2.13+450&&mouseY>height/6.6&&mouseY<height/6.6+30){
+    fill('#503D2E');
+    noStroke();
+    //rect(960, 100, 90, 30, 15);
+    rect(width/2.13+360, height/6.6, 90, 30, 15);
+    fill(255);
+    text('Customize', width/2.13+405, height/6.6+20);
+  }
 
 }
 
 function mousePressed(){
-  if(mouseX>600&&mouseX<690&&mouseY>100&&mouseY<130){
+  if(mouseX>width/2.13&&mouseX<width/2.13+90&&mouseY>height/6.6&&mouseY<height/6.6+30){
     fill('#E3A72F');
     window.location.href = "index.html";
   }
 
-  if(mouseX>720&&mouseX<810&&mouseY>100&&mouseY<130){
+  if(mouseX>width/2.13+120&&mouseX<width/2.13+210&&mouseY>height/6.6&&mouseY<height/6.6+30){
     fill('#058789');
     window.location.href = "use.html";
   }
 
-  if(mouseX>840&&mouseX<930&&mouseY>100&&mouseY<130){
+  if(mouseX>width/2.13+240&&mouseX<width/2.13+330&&mouseY>height/6.6&&mouseY<height/6.6+30){
     fill('#D54B1A');
     window.location.href = "comment.html";
   }
 
-  if(mouseX>960&&mouseX<1050&&mouseY>100&&mouseY<130){
+  if(mouseX>width/2.13+360&&mouseX<width/2.13+450&&mouseY>height/6.6&&mouseY<height/6.6+30){
     fill('#503D2E');
     window.location.href = "customize.html";
   }
